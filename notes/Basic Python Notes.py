4f85673a35ab7881881bf914ef0621cf341b1552
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2018-10-24 09:46:46 -0700

    Added our first notes
    10/24/18

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -10,7 +10,6 @@ print("Look at what happens here. Is there any space?")
 print()
 print()
 print("There should be a couple blank lines here.")
-
 # Math
 print(3 + 5)
 print(5 - 2)
@@ -26,3 +25,32 @@ print("Here is another one...")
 print(6 % 2)
 print(5 % 2)
 print(11 % 4)  # Modulus (Remainder)
+
+# Powers
+# What is 2^20?
+print(2 ** 20)
+
+# Taking input
+# %s. is the symbol for "String"
+name = input("What is your name?")
+print("Hello, %s." % name)
+
+age = input("How old are you? >_")
+print("%s?!? You belong in a museum." % age)
+print()
+print("%s is really old. They are %s years old." % (name, age))
+
+# Variable Assignments
+car_name = "Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 16
+car_miles_per_gallon = 0.01
+
+# Make it print "I have a car called Wiebe Mobile. It is a Tesla."
+
+print("I have a car called %s. It is a %s." % (car_name, car_type))
+
+# Recasting
+real_age = int(input("How old are you again?"))
+hidden_age = real_age + 5
+print("This is your real age: %d" % hidden_age)
