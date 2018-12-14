140feef75c5be06b3f1a81e152ae4b2ba59b13b2
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2018-12-14 10:41:37 -0800

    Free day before break
    12/14/18

--- notes/Practice game.py
+++ notes/Practice game.py
@@ -1,11 +1,14 @@
 import random
-stats = [0, 1, 2, 3, 4]  # 0 = hp, 1 = damage_resist, 2 = magic_resist, 3 = speed, 4 = strength, 5 = mana
+stats = [0, 1, 2, 3, 4, 5, 6, 7]  # 0 = hp, 1 = damage_resist, 2 = magic_resist, 3 = speed, 4 = strength, 5 = mana,
+# 6 = intelligence, 7 = luck
 # Average hit = 10 pts, standard hp = 500
 name = input("Who are you?")
 # character_class = input("WHAT are you? (You can only say 'I don't know' for now)")
 equips = [0, 1, 2, 3, 4, 5]
 bag_o_items = []
-
+spells = ["Fire spell", "Healing spell", "Ice spell"]
+effects = ["flame", "freeze", "poison"]
+stats[7] = random.randint(-13, 7)
 """
 if "wizard" or "sorcerer" in character_class:
     stats[1] = 3
@@ -28,6 +31,25 @@ if "I don't know" in character_class:
     stats[4] = 7 # strength
     stats[5] = 200 # mana
 """
-character_class = "wizard"
+
+def you_attacked(a, b, c, e_l, ):
+    if action.lower in "attack":
+        print("You attacked it!")
+        enemy_hp = enemy_hp - (stats[4] - enemy_luck)
+
+
+
+actions = ["attack", "spell", "defend"]
+def battle ():
+    enemy_hp = random.randint(200, 600)
+    enemy_strength = random.randint(1, 10)
+    enemy_intelligence = (random.randint (1, 10))
+    enemy_resist = random.randint(1, 10)
+    enemy_luck = random.randint(-13, 7)
+    while enemy_hp > 0:
+        action = input("What is your next move? ie:attack, spell, defend")
+        enemy_action = random.choice(actions)
+
+
 
 
