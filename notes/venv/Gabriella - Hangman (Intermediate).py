13b357924f072a891ba30cc9c2017e34b7dfb1ca
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2019-01-14 09:46:12 -0800

    Still working on Hangman...Display errors "Index assignment out of range"
    1/14/19

--- notes/venv/Gabriella - Hangman (Intermediate).py
+++ notes/venv/Gabriella - Hangman (Intermediate).py
@@ -4,36 +4,42 @@ word_bank = ["Super", "Word", "Bow", "Read", "Maverick", "Awesome", "Trapdoor", 
              "River", "Discombobulated", "Zippy", "Gazebo", "Intermediate", "Croquet", "Aerobics", "Anaerobic"]
 
 word = random.choice(word_bank)
+word_lower = list(word.lower())
 letters_guessed = []
 guesses = 8
-blank = ["_" * len(word)]
+
+
 case = "lower"
 win = False
-display = "".join(blank)
-def dev_details():
-    print("WORD = " + word + ", %s LETTERS" % len(word))
-    print(blank)
-    print(display)
-    print("GUESSES = " + str(guesses))
+
+print("WORD = " + word + ", %s LETTERS" % len(word))
+print("GUESSES = " + str(guesses))
 
 
-dev_details()
 
 while win == False and guesses > 0:
     print()
     print(letters_guessed)
-
+    blank = ["_" * len(word)]
     guess_letter = input("Give me a letter:")
     if guess_letter.lower in letters_guessed:
         print("You have already guessed this letter.")
     elif guess_letter.lower() in word.lower():
         print("Yes, %s is in the word." % guess_letter)
         letters_guessed.append(guess_letter.lower())
-        blank[(word.lower()).index(guess_letter.lower())] = word[(word.lower()).index(guess_letter.lower())]
+        #  blank[(word.lower()).index(guess_letter.lower())] = word[(word.lower()).index(guess_letter.lower())]
+
+        print(blank)
+        for i in range(len(word)):
+            if word_lower[i] in letters_guessed:
+                blank[i] = word[i]
+                print(blank)
+            else:
+                blank[i] = "_"
+                print(blank)
+        display = "".join(blank)
     elif guess_letter not in word.lower() or guess_letter not in word.upper():
         print("No, %s is not in the word. Sorry!" % guess_letter)
         letters_guessed.append(guess_letter.lower())
         guesses -= 1
     print(display)
-
-
