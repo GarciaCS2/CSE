ee29ebb1b940597662d14dccb753d773fe4f5315
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2019-01-16 09:45:22 -0800

    Hangman still finished, but fixing it up
    1/16/19

--- notes/Arcade Deluxe.py
+++ notes/Arcade Deluxe.py
@@ -1,9 +1,35 @@
 import random
 import string
+points = 8
+# Command banks
+name = input("What is your username?")
+
+cancel_items = ["cancel", "never mind", "nevermind", "go back"]
+show_stats_items = ["show", "stats", "show stats"]
+print("Welcome to the Arcade!")
+
+def show_stats():
+    print(name.upper())
+    print("You have %s points" % points)
+
+
+
+def command():
+    do = input("Type in a command")
+    if do in cancel_items:
+        print("Ok, back to game.")
+    elif do in show_stats_items:
+        show_stats
+
+
+def choose_game():
+    print("Your choices are: Hangman, Lucky 7's, Guess Game,")
+    choice = input("Which game would you like to play? ")
+
+# HANGMAN
 word_bank = ["Super", "Word", "Bow", "Read", "Maverick", "Awesome", "Trapdoor", "Pizzicato", "Embezzled", "Garnish",
              "Math", "Python", "Variable", "Concatenation", "Awesome.com", "Fjord", "Crypt", "Bagpipes", "Haiku",
              "Jazzy", "Banjo", "River", "Discombobulated", "Zippy", "Gazebo", "Intermediate", "Croquet", "Aerobics",
-             "Meme", "Dream", "Zebra", "Quick", "The quick brown fox jumped over the lazy dog.", "What do you meme?"
              "Gloves", "Cats vs. Dogs", "Hangman", "Leaflet", "Champagne", "Haircut", "Minimum", "Maximum", "Cat"
              "Anaerobic", "Mr.Mayonnaise", "Mc.Donald's", "Grue", "import random", "Alphabet"]  # The word bank!
 
@@ -11,7 +37,6 @@ word_bank = ["Super", "Word", "Bow", "Read", "Maverick", "Awesome", "Trapdoor", 
 word = random.choice(word_bank)
 word_lower = list(word.lower())
 letters_guessed = []
-guesses = 8
 blank = []
 punctuation_with_space = list(string.punctuation)
 punctuation_with_space.append(" ")
@@ -26,10 +51,10 @@ for i in range(len(word)):
     blank.append("_")  # Assemble list
 
 # Playing
-while guesses > 0 and not win:
+while points > 0 and not win:
     print()
     print(letters_guessed)
-    print("You have %s guesses." % guesses)
+    print("You have %s guesses." % points)
     guess_letter = input("Give me a letter:")
     if guess_letter.lower() in letters_guessed:
         print("You have already guessed '%s'." % guess_letter)
@@ -39,7 +64,7 @@ while guesses > 0 and not win:
     elif guess_letter not in word.lower() or guess_letter not in word.upper():  # Wrong guess
         print("No, %s is not in the word. Sorry!" % guess_letter)
         letters_guessed.append(guess_letter.lower())
-        guesses -= 1
+        points -= 1
     for i in range(len(word)):  # Assemble list again for display
         if word_lower[i] in letters_guessed:
             blank[i] = word[i]
@@ -52,9 +77,8 @@ while guesses > 0 and not win:
     if display == word:
         win = True
 
-#  Did you win or lose?
 print("The word was %s!" % word)
 if win:
-    print("You won! You had %s guesses left!" % guesses)
+    print("You won! You had %s guesses left!" % points)
 else:
     print("You lost. Try again next time.")
