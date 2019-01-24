0d533f497fd0c65ace5bad401ff4502cffae4069
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2019-01-24 09:45:49 -0800

    More practice with OOP
    1/24/19

--- notes/Object and Dictionary Practice.py
+++ notes/Object and Dictionary Practice.py
@@ -2,17 +2,12 @@ import random
 #  OOP - https://realpython.com/python3-object-oriented-programming/
 #  Dictionary - https://docs.python.org/3/tutorial/datastructures.html?highlight=dictionary
 class Snop:
-    def __init__(self, name, age, cool, likes, dislikes, stuff, tokens, niceness):
+    def __init__(self, name, age, cool):
 
         species = 'mammal'
         self.cool = True
         self.name = name
         self.age = age
-        self.likes = likes
-        self.dislikes = dislikes
-        self.stuff = stuff
-        self.money = tokens
-        self.niceness = niceness
     pass
 
 jeff = Snop('Jeff', 14, True)
@@ -36,11 +31,52 @@ def find_cool_snop():
 find_cool_snop()
 
 
-def gen_snops():
-    names = ["Jason", "Jeff", "Randy", "Sunny", "Fluffy", "Mo", "Larry", "Mary", "Bo"]
-    items = {"Bouncy Ball":2, "Bracelet": 7, "Pencil":1, "Gifts":4, "Pet Fluff":10}
-    for i in range(random.int(1, 15)):
-        like =
-    list.append(snop_population, Snop(random.choice(names), random.int(20, 30), random.choice(True, False),
-                                      like, not_like, stuff, tokens, niceness)
+class Snop:
+    def __init__(self, name, age, cool, likes, dislikes, stuff, tokens, niceness):
 
+        species = 'mammal'
+        self.cool = True
+        self.name = name
+        self.age = age
+        self.likes = likes
+        self.dislikes = dislikes
+        self.stuff = stuff
+        self.money = tokens
+        self.niceness = niceness
+    pass
+
+
+names = ["Jason", "Jeff", "Randy", "Sunny", "Fluffy", "Mo", "Larry", "Mary", "Bo", "Lovely", "Beatrice", "Ari", "Mason",
+         "Grue"]
+items_values = {"Bouncy Ball":2, "Bracelet": 7, "Pencil":1, "Gifts":4, "Pet Fluff":10}
+items = ["Bouncy Ball", "Bracelet", "Pencil", "Gifts", "Pet Fluff"]
+like = []
+not_like = []
+possessions = []
+thing = "Nothing"
+for i in range(random.randint(1, len(items))):
+    thing = random.choice(items)
+    if thing not in like:
+        like.append(thing)
+for i in range(random.randint(1, len(items))):
+    thing = random.choice(items)
+    if thing not in not_like and thing not in like:
+        not_like.append(thing)
+for i in range(random.randint(1, len(items))):
+    trufalse = [True, False]
+    have = random.choice(trufalse)
+    thing = random.choice(items)
+    if have:
+        possessions.append(thing)
+multiplier = random.randint(1, 5)
+value = random.randint(1, 10)
+richness = value * multiplier
+nice = random.randint(1, 10)
+new_snop = Snop(random.choice(names), random.randint(20, 30), random.choice(trufalse), like, not_like, possessions,
+                richness, nice)
+list.append(snop_population, new_snop)
+
+
+print()
+print("New scenario:")
+print("There are %s Snops." % len(snop_population))
