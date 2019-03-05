20ec99fb0e893817beca802832aee5dfa375e1dc
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2019-03-05 09:45:15 -0800

    More progress on items
    3/5/19

--- Gabriella - Item.py
+++ Gabriella - Item.py
@@ -6,7 +6,21 @@ class Item(object):
 
 
 class Equipment(Item):
-    def __init__(self, name, description, condition='New', function):
-        super(Equipment, self).__init__(name, description)
+    def __init__(self, name, description, location, weight, condition='NEW'):
+        super(Equipment, self).__init__(name, description, location)
+        self.weight = weight
         self.condition = condition
-        self.function = function
+
+
+class Weapon(Equipment):
+    def __init__(self, name, description, location, weight, attack_power, reach):
+        super(Weapon, self).__init__(name, description, location, weight)
+        self.attack_power = attack_power
+
+        self.range_or_reach = reach
+
+
+class Generic(Weapon):
+    def __init__(self, name, description, location, weight, attack_power, reach, type):
+        super(Generic, self).__init__(name, description, location, 'MODERATE', 70, reach)
+        self.type = type
