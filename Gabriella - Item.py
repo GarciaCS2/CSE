0f48fcf7b849dd18a9ad90af764d753379785868
GarciaCS2 <42385716+GarciaCS2@users.noreply.github.com>
2019-03-06 09:45:26 -0800

    More work done on OOP Items
    3/6/19

--- Gabriella - Item.py
+++ Gabriella - Item.py
@@ -16,11 +16,22 @@ class Weapon(Equipment):
     def __init__(self, name, description, location, weight, attack_power, reach):
         super(Weapon, self).__init__(name, description, location, weight)
         self.attack_power = attack_power
-
         self.range_or_reach = reach
 
 
-class Generic(Weapon):
-    def __init__(self, name, description, location, weight, attack_power, reach, type):
-        super(Generic, self).__init__(name, description, location, 'MODERATE', 70, reach)
-        self.type = type
+class Standard(Weapon):  # INSTANTIABLE Generic Weapon
+    def __init__(self, name, description, location, kind):
+        super(Standard, self).__init__(name, description, location, 'MODERATE', 70, 'DEFAULT')
+        self.kind = kind  # "MELEE" or "RANGED"
+
+
+class Maxe(Weapon):  # INSTANTIABLE Magical War axe (Maxe..M-axe >>> Magic-Axe)
+    def __init__(self, location, imprint, curse):
+        super(Maxe, self).__init__("Magical Marble War Axe", "A lovely polished war axe made of marble, purple designs "
+                                                             "painted on it like a vase...and enchanted like a curse.",
+                                   location, 'MODERATE', 200, 'EXTENDED')
+        self.imprint = imprint  # You can claim the weapon as your own and you shall be able to summon it at will
+        self.curse = curse  # You can set a curse you want to use on your enemies
+
+
+sword = Standard("Some sword.", "Just a generic sword thing.", "INSERT ROOM HERE", 'MELEE')
